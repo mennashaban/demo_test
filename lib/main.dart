de8152891a39653f7d81ba491042b86a9c57e71c
mennashaban <20201700864@cis.asu.edu.eg>
2024-04-04 22:30:53 +0200

    first one

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:demo_2/upload_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'test uploading',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
---
+++ lib/upload_screen.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_v2/tflite_v2.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+  bool _isImageLoaded = false;
+  String _result = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_quant_gp.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  classifyImage(File image) async {
+    var result = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 1,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _result = result![0]['label'];
+    });
+  }
+
+  Future getImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (image != null) {
+        _image = File(image.path);
+        _isImageLoaded = true;
+        classifyImage(_image!);
+      }
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("upload ur image"),
+      ),
+      body: Center(
+        child: _isImageLoaded
+            ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.file(_image!),
+            SizedBox(height: 20),
+            Text(
+              'Category: $_result',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        )
+            : Text('No image selected.'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+}
